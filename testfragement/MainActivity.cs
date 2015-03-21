40ec2e9dc6a74d6622678f687aa230bb657aa9e2
FRE3X <Callot.antoine@gmail.com>
2015-03-21 13:11:06 +0100

    premiers commit

---
+++ testfragement/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Support.V4; 
+using Android.Widget;
+using Android.OS;
+
+namespace testfragement
+{
+	[Activity (Label = "testfragement", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				FragmentTransaction transaction = FragmentManager.BeginTransaction(); 
+				Dialog dialog = new Dialog(); 
+				dialog.Show(transaction, "dialog_frame"); 
+			};
+		}
+	}
+}
+
+
